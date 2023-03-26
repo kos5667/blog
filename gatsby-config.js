c2e186ad7afad239a1ec1416adf5360e8ef8c35f
kos5667 <kos5667@gmail.com>
2023-03-26 23:25:34 +0900

    temp gastby

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      // twitter: `kylemathews`,
     },
   },
   plugins: [
