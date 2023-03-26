0b8235526dc2cc27dc84afb85d2ae76e6c93ffe5
kos5667 <kos5667@gmail.com>
2023-03-26 23:37:56 +0900

    edit introduce box

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Don's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Don's`,
+      summary: ` and daily Blog`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
--- src/components/bio.js
+++ src/components/bio.js
@@ -44,11 +44,11 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+          {/*<a href={`https://twitter.com/${social?.twitter || ``}`}>*/}
+          {/*  You should follow them on Twitter*/}
+          {/*</a>*/}
         </p>
       )}
     </div>
