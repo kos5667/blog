7b3234ab5c67011b7ac68f7cb1f6d4802b0afd6b
kos5667 <kos5667@gmail.com>
2023-03-31 23:33:32 +0900

    - add categories.tsx - add blog read me

---
+++ Blog-README.md
@@ -0,0 +1,15 @@
+### BLOG
+
+### Package
+
+    ├── About : Profile
+    ├── Book Journal :
+    ├── Tech Blog :
+    ├── Java
+    ├── Spring & Spring Boot
+    │
+    ├── Node.js(Express)
+    ├── Java
+    ├── Java
+    ├── Java
+    ├── Book Journal :
---
+++ src/pages/categories.tsx
@@ -0,0 +1,9 @@
+// gatsby-plugin-graphql-codegen이 생성해 준 타입을 가져 온다.
+// import { CategoriesQuery } from "../../graphql-types"
+// import React from "react";
+// import {PageProps} from "gatsby";
+//
+// // 더 이상 DataProps를 선언할 필요가 없다. PageProps<>에 그대로 넣어 준다.
+// const Categories: React.FC<PageProps<CategoriesQuery>> = {
+//     // 생략
+// }
